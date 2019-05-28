29eb0b8be541c9349c9959c9aab2f378cb5aa2d8
ouyinheng <2675379992@qq.com>
2019-05-28 18:01:26 +0800

    '新增一个播放页面'

--- src/views/itd/index.vue
+++ src/views/itd/index.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="itd">
-     <o-video></o-video>
-     <music-item></music-item>
+     <!-- <o-video></o-video> -->
+     <!-- <music-item></music-item> -->
+     <testmusic></testmusic>
   </div>
 </template>
 
 <script>
 import ovideo from '@/components/oVideo.vue';
 import musicitem from '@/components/music-item.vue';
+import testmusic from './testmusic.vue'
 export default {
   name: 'itd',
   components: {
     'o-video': ovideo,
-    'music-item': musicitem
+    'music-item': musicitem,
+    testmusic
   }
 }
 </script>
